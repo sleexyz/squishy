80b935acb82579c47265968649cf68e5cf21822a
Sean Lee <freshdried@gmail.com>
2017-05-22 01:57:50 -0400

    parens

--- src/Squishy/Parser.hs
+++ src/Squishy/Parser.hs
@@ -5,7 +5,7 @@ import Control.Applicative
 import Data.Attoparsec.Text (Parser)
 import qualified Data.Attoparsec.Text as P
 import Data.Text
-import Data.Char (isSpace)
+import qualified Data.Char as C
 
 import Squishy.Types
 
@@ -14,29 +14,39 @@ parseAST = P.parseOnly astParser
 
 astParser :: Parser AST
 astParser =
-      letParser
+      parenParser
+  <|> letParser
   <|> primParser
   <|> varParser
-  where
-    letParser :: Parser AST
-    letParser = do
-      P.string "let"
-      P.skipSpace
-      n <- P.takeTill isSpace
-      P.skipSpace
-      P.char '='
-      P.skipSpace
-      x <- astParser
-      P.skipSpace
-      P.string "in"
-      P.skipSpace
-      y <- astParser
-      P.skipSpace
-      return $ Let n x y
 
-    primParser :: Parser AST
-    primParser = (P.string "True" >> return (Prim True))
-      <|> (P.string "False" >> return (Prim False))
+letParser :: Parser AST
+letParser = do
+  _ <- P.string "let"
+  P.skipSpace
+  n <- P.takeTill (\c -> C.isSpace c || c == '=')
+  P.skipSpace
+  _ <- P.char '='
+  P.skipSpace
+  x <- astParser
+  P.skipSpace
+  _ <- P.string "in"
+  P.skipSpace
+  y <- astParser
+  P.skipSpace
+  return $ Let n x y
 
-    varParser :: Parser AST
-    varParser = Var <$> (P.takeWhile (/= ' ')) <*> pure ()
+parenParser :: Parser AST
+parenParser = do
+  _ <- P.char '('
+  P.skipSpace
+  n <- astParser
+  P.skipSpace
+  _ <- P.char ')'
+  return n
+
+primParser :: Parser AST
+primParser = (P.string "True" >> return (Prim True))
+  <|> (P.string "False" >> return (Prim False))
+
+varParser :: Parser AST
+varParser = Var <$> (P.takeWhile C.isAlpha) <*> pure ()
--- test/SquishySpec.hs
+++ test/SquishySpec.hs
@@ -49,19 +49,31 @@ spec = do
         `shouldBe` Right (Let "foo" (Prim True) (Prim False))
       parseAST "let x = True in\nlet y = False in y"
         `shouldBe` Right (Let "x" (Prim True) (Let "y" (Prim False) (Var "y" ())))
+    describe "with parens" $ do
+      it "should still work" $ do
+        parseAST "(True)" `shouldBe` Right (Prim True)
+        parseAST "( True )" `shouldBe` Right (Prim True)
+        parseAST "( (True) )" `shouldBe` Right (Prim True)
+        parseAST "let x = (let y = False in y) in x"
+          `shouldBe` Right (Let "x" (Let "y" (Prim False) (Var "y" ())) (Var "x" ()))
 
   describe "resolveNames" $ do
     it "should work for ASTs without binding" $ do
       resolveNames       (Prim True)
         `shouldBe` Right (Prim True)
+
       resolveNames       (Prim True)
         `shouldBe` Right (Prim True)
 
     it "should work for ASTs with binding" $ do
       resolveNames       (Let "x" (Prim True) (Let "y" (Prim False) (Var "x" ())))
         `shouldBe` Right (Let "x" (Prim True) (Let "y" (Prim False) (Var "x" 0)))
+
       resolveNames       (Let "x" (Prim True) (Let "y" (Prim False) (Var "y" ())))
         `shouldBe` Right (Let "x" (Prim True) (Let "y" (Prim False) (Var "y" 1)))
+
+      resolveNames       (Let "x" (Let "y" (Prim False) (Var "y" ())) (Var "x" ()))
+        `shouldBe` Right (Let "x" (Let "y" (Prim False) (Var "y" 0)) (Var "x" 0))
 
     it "should work for ASTs with binding and shadowing" $ do
       resolveNames       (Let "x" (Prim True) (Let "x" (Prim False) (Var "x" ())))
